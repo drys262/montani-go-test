253c782264514061242a3c8d17415af44da9074b
drys262 <dkchave0987@gmail.com>
2024-06-15 15:55:35 +0800

    feat: add dynamic params on cf

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
     app := gin.Default()
     app.GET("/", func(c *gin.Context) {
         c.JSON(http.StatusOK, gin.H{
-            "message": "Hello World v2!",
+            "message": "Hello World V3!",
         })
     })
     app.Run(":8080")
