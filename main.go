2d2cb8714ae220de7ec6f7994efb6d7ee88ec21f
drys262 <dkchave0987@gmail.com>
2024-06-15 15:23:08 +0800

    feat: add task-definition file & update api

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
     app := gin.Default()
     app.GET("/", func(c *gin.Context) {
         c.JSON(http.StatusOK, gin.H{
-            "message": "Hello World!",
+            "message": "Hello World v2!",
         })
     })
     app.Run(":8080")
